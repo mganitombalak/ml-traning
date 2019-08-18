742ec8e67d9a1e2bd223aaf6298bc2636ff4509e
mganitombalak <mgani.tombalak@yahoo.com>
2019-08-18 21:14:23 +0300

    Gradient Descent Sample

--- test.m
+++ test.m
@@ -1,15 +1,30 @@
-x=[1 0;1 1;1 2;1 3]
-y=[2.5;3;3.7;4]
-m = length(y)
-theta=[0;0]
-JTheta=[0 0 0]
-lr=0.01
-for iter =1:100
-h=theta(1) + (theta(2)*x)
-J= (1/2*m) * sum(h-y).^2
-theta_zero=theta(1)-(lr/m)*sum(h-y)
-theta_one=theta(2)-(lr/m)* x(:,2)'*h
-theta =[theta_zero,theta_one]
-JTheta(end+1,:)=[J(1) theta_zero(1) theta_one(1)]
+x=[1;2;3;4];
+y=[2;4;6;8];
+figure(1)
+plot(x,y,'rx');
+teta=[0;0];
+lr=0.04;
+m=length(y);
+iteration=500
+jteta=zeros(iteration,3)
+for iter=1:iteration
+h=teta(1) + (teta(2)*x);
+diff=h-y
+j= (1/2*m)*sum(diff).^2;
+teta_zero = teta(1) - (lr/m)*sum(diff)
+teta_one = teta(2) - (lr/m)*sum(diff.*x);
+teta=[teta_zero;teta_one]
+jteta(iter,:)=[j teta_zero teta_one]
 endfor
-mesh(J)
+tetatemp= jteta(iteration,2:3)
+h2=tetatemp(1) + (tetatemp(2)*x);
+hold on
+plot(h2)
+hold off
+figure(2)
+plot3(jteta)
+xlabel("teta0")
+ylabel("teta1")
+zlabel("J")
+
+
